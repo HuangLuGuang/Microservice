9fa1355229cb675a04acfca5a942cc83b9754909
huanglg <835923123@qq.com>
2019-07-17 23:24:12 +0800

    4

--- unit2/4.py
+++ unit2/4.py
@@ -3,19 +3,20 @@
 # @author  : Huanglg
 # @fileName: 3.py
 # @email: luguang.huang@mabotech.com
-from flask import Flask, g
-import yaml
-
-def yamlify(data, status=200, headers=None):
-    _headers = {"Content-Type": "application/x-yaml"}
-    if headers is not None:
-        _headers.update(headers)
-    return yaml.safe_dump(data), status, _headers
+from flask import Flask, jsonify, g, request
 
 app = Flask(__name__)
 
+@app.before_request
+def authenticate():
+    if request.authorization:
+        g.user = request.authorization['username']
+    else:
+        g.user = 'Anonymous'
+
 @app.route('/api')
 def my_microservice():
-    return yamlify(['hello', 'yaml', 'world!'])
+    return jsonify({'hello': g.user})
+
 if __name__ == '__main__':
     app.run()
